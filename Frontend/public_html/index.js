5c3708a5b4c03871bb9ec9f54a11b744673c386e
Yamataw <maxime.henri@ynov.com>
2022-11-22 14:08:56 +0100

    Login  + one post

--- Frontend/public_html/index.js
+++ Frontend/public_html/index.js
@@ -1,9 +1,61 @@
-function handleclick(route) {
-    let url = "http://localhost:3000/" + route;
 
-    axios.get(url)
-        .then((response) => document.getElementById("image").src = response.data[0].url)
-        .catch((error) => console.log(error))
+function test(){
+    console.log('test')
+}
+
+function handleLogin(){
+    console.log('test')
+    var data = new FormData();
+    let username = document.getElementById('usernameForm')
+    let password = document.getElementById('passwordForm')
+    console.log(username,password)
+    data.append('username', username.value);
+    data.append('password', password.value);
+
+    var config = {
+        method: 'post',
+        url: 'http://127.0.0.1: 8000/login/',
+        data : data
+    };
+
+    axios(config)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data));
+            console.log(response);
+            if(response.status === 200) {
+                console.log('cc ça marche');
+                window.localStorage['token'] = JSON.stringify(response.data);
+                window.location.href = "HomePage.html"
+            }
+
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+}
+
+
+function handleclick(route) {
+    var data = '';
+    var token = window.localStorage['token'];
+    var config = {
+        method: 'get',
+        url: 'http://127.0.0.1:8000/' + route,
+        headers: {
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY5MTI5ODQxLCJpYXQiOjE2NjkxMDgyNDEsImp0aSI6IjJmY2M2ZGNlYmEyNTQ2ZGY5ZWNmMTY0ODQ0YjkwM2QzIiwidXNlcl9pZCI6MSwidXNlcm5hbWUiOiJhZG1pbiJ9.13kOd3dzfILFPRwT9Ed5gGc9PqVfPR-3z9BF1pV6DT4'
+        },
+        data : data
+    };
+
+    axios(config)
+        .then(function (response) {
+            document.getElementById('image').src = response.data.image;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
 }
 
 function handleclickPost(route) {

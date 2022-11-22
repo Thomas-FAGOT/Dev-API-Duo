741af293da32c156ed8cf55dbe705f52ace7bb53
Thomas-FAGOT <thomasfagot@ynov.com>
2022-11-22 11:27:49 +0100

    ajout des route des apis sur les boutons

--- Frontend/public_html/index.js
+++ Frontend/public_html/index.js
@@ -5,3 +5,31 @@ function handleclick(route) {
         .then((response) => document.getElementById("image").src = response.data[0].url)
         .catch((error) => console.log(error))
 }
+
+function handleclickPost(route) {
+    let url = "http://localhost:3000/" + route;
+    let img = document.getElementById("image").src;
+    let com = document.getElementById("commentaire").ariaValueMax;
+    let arrayDataName = {
+        "url": "",
+        "date": null,
+        "image": img,
+        "content": com,
+        "user": ""
+    }
+    
+    axios.post(url, arrayDataName)
+        .then((response) => console.log(response))
+        .catch((error) => console.log(error))
+}
+
+function handleclickPut(route) {
+    let url = "http://localhost:3000/" + route;
+
+    axios.put(url, arrayDataName)
+        .then((response) => {
+            document.getElementById("image").src = response.data[0].url;
+            docu
+        })
+        .catch((error) => console.log(error))
+}
